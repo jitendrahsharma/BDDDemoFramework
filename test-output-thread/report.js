6f5432010831e391a257be117418694915d1d2d9
jitendra.sharma <jitendra.sharma@eclinicalworks.com>
2021-08-04 11:03:12 -0400

    First commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "f166ee7a-2911-46da-a1cc-a910322a8ef1",
+    "feature": "Orangehrm Login Page feature",
+    "scenario": "Verify forgot password link",
+    "start": 1626337686519,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1626337693792,
+    "className": "passed"
+  },
+  {
+    "id": "5ff5e934-d369-491f-bc64-37ace30f9fa1",
+    "feature": "This file contains scenarios for the Users",
+    "scenario": "To Verify able to add user in the application",
+    "start": 1626337732495,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1626337762647,
+    "className": "passed"
+  },
+  {
+    "id": "b5f46c19-cb0e-4f6b-89b1-08d4b3d30e39",
+    "feature": "Orangehrm Login Page feature",
+    "scenario": "verify login with corrent credentials",
+    "start": 1626337693797,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1626337706588,
+    "className": "passed"
+  },
+  {
+    "id": "04324ca9-ce6e-43d2-878a-da0d204e4def",
+    "feature": "This file contains scenarios for the Organization features",
+    "scenario": "To verify the functionality of add the location in the organizations",
+    "start": 1626337706596,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1626337732488,
+    "className": "passed"
+  },
+  {
+    "id": "3ccb2029-5da5-4d58-93fd-54ce7f3c28c7",
+    "feature": "This file contains scenarios for the Job Titles",
+    "scenario": "To verify the functionality add the job title",
+    "start": 1626337624634,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1626337650715,
+    "className": "passed"
+  },
+  {
+    "id": "d1c85f34-502b-42ae-9d41-c31c3ff12bb2",
+    "feature": "This file contains scenarios for the Job Titles",
+    "scenario": "To verify the functionality add the pay grade",
+    "start": 1626337650724,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1626337678685,
+    "className": "passed"
+  },
+  {
+    "id": "e14d515a-83a0-41c1-9dcb-65365b9a5640",
+    "feature": "Orangehrm Login Page feature",
+    "scenario": "Verify Login page title",
+    "start": 1626337678699,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1626337686515,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
